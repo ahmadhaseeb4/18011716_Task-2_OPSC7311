935e196f48886d03fec7310d95354ff2345a5b50
Muhammad Mubeen Hamid <muhammadmubeenhamid@gmail.com>
2020-10-27 23:43:33 +0200

    Initial commit

---
+++ app/src/main/java/com/task2/a18011716_task_2_opsc7311/MainActivity.java
@@ -0,0 +1,14 @@
+package com.task2.a18011716_task_2_opsc7311;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
